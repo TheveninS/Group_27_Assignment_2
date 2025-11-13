84a8aa7aae3b509c9f8eb3ff8b1dd23f2dd788ca
Thevenin.Syrill <thevenin.s@northeastern.edu>
2025-11-13 18:52:18 -0500

    Added some classes to complete the backend for the assignment

--- RangePricingApplication/src/TheBusiness/CustomerManagement/CustomerProfile.java
+++ RangePricingApplication/src/TheBusiness/CustomerManagement/CustomerProfile.java
@@ -19,50 +19,58 @@ public class CustomerProfile extends Profile {
 
     ArrayList<Order> orders;
     ArrayList<Market> markets;
-
     Person person;
 
     public CustomerProfile(Person p) {
         super(p);
         person = p;
         orders = new ArrayList();
-
     }
     
     @Override
     public String getRole(){
         return "Sales";
     }
-    
-    
 
     public int getTotalPricePerformance() {
-
-        //for each order in the customer orderlist 
-        //calculate order price performance and add it to the sum
-        return 0;
+        int sum = 0;
+        for (Order o : orders) {
+            sum += o.getOrderPricePerformance();
+        }
+        return sum;
     }
 
     public int getNumberOfOrdersAboveTotalTarget() {
-        //for each order in the customer order list 
-        //calculate if order is positive (actual order total is greater than sum of item targets
-        //if yes then add 1 to total 
         int sum = 0;
         for (Order o : orders) {
             if (o.isOrderAboveTotalTarget() == true) {
                 sum = sum + 1;
             }
         }
-
         return sum;
     }
 
     public int getNumberOfOrdersBelowTotalTarget() {
-        return 0;
+        int sum = 0;
+        for (Order o : orders) {
+            if (!o.isOrderAboveTotalTarget()) {
+                sum = sum + 1;
+            }
+        }
+        return sum;
     }
-    //for each order in the customer order list 
-    //calculate if order is negative
-    //if yes then add 1 to total 
+    
+    public int getTotalRevenue() {
+        int sum = 0;
+        for (Order o : orders) {
+            sum += o.getOrderTotal();
+        }
+        return sum;
+    }
+    
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
 
     public boolean isMatch(String id) {
         if (person.getPersonId().equals(id)) {
@@ -87,5 +95,4 @@ public class CustomerProfile extends Profile {
     public Person getPerson() {
         return person;
     }
-
 }
